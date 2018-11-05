6f9d3747fa184b4293bc92962a76dab1bdec7946
Dimitrios Bendilas <dimitrios@transifex.com>
2018-11-05 02:01:24 +0200

    Allow cli to run with default config
    Users can now call the CLI command without specifying a config file.
    In this case, the newly added default config is used.

--- cli.py
+++ cli.py
@@ -9,16 +9,18 @@ from temcheck.main import TemCheck
 
 @click.command()
 @click.option('-p', '--pr-url', required=True, type=str)
-@click.option('-c', '--config-file', required=True, type=str)
+@click.option('-c', '--config-file', required=False, type=str)
 @click.option('--details-url', required=False, type=str)
-def main(pr_url, config_file, details_url=None):
+def main(pr_url, config_file=None, details_url=None):
     """Run all checks described in `config_file` for the PR on the given URL.
 
     :param str pr_url: the URL of the pull request as retrieved from the CI
     :param str config_file: the path of the configuration file,
-        formatted as as described in CheckSuite (in JSON format)
+        formatted in YAML, as found in contrib/config/sample.yml
     :param str details_url: the URL to visit for more details about the results
     """
+    if not config_file:
+        config_file = './contrib/config/default.yml'
     try:
         with open(config_file, 'r') as f:
             try:
